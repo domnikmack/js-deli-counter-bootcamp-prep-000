e996cc928230f2bfeac81d3bc6df963a26961715
Domnik Mack <domnikmack@gmail.com>
2017-09-25 14:45:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing (katzDeliLine) {
   if (katzDeliLine) {
-    return 'Currently serving 'katzDeliLine.shift();'.';
+    return 'Currently serving ' + katzDeliLine.shift(); + '.';
   }
 
 }
