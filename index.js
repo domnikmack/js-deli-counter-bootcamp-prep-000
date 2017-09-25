67d585756338246af9e4fe6721a3aed99647a3a0
Domnik Mack <domnikmack@gmail.com>
2017-09-25 15:16:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing (katzDeliLine) {
-  if (katzDeliLine) {
+  if (katzDeliLine.lenght > 0) {
     return 'Currently serving ' + katzDeliLine.shift() + '.';
   } else {
     return 'There is nobody waiting to be served!';
